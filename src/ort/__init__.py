af33dc3399473d69bd743297ea92f3629d6770c2
Helio Chissini de Castro <helio.chissini.de.castro@cariad.technology>
2025-02-12 19:42:31 +0100

    feat: Test infrastructure
    Signed-off-by: Helio Chissini de Castro <helio.chissini.de.castro@cariad.technology>

--- src/ort/__init__.py
+++ src/ort/__init__.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 from ort.analyzer_configuration import OrtAnalyzerConfigurations
 from ort.curations import OrtCurations
 from ort.license_classifications import OrtLicenseClassifications
-from ort.ort_configuration import OrtConfiguration
+from ort.ort_configuration import OrtConfiguration, Scanner, Severity, Storages
 from ort.package_configuration import OrtPackageConfiguration
 from ort.package_manager_configuration import OrtPackageManagerConfigurations
 from ort.package_managers import OrtPackageManagers
@@ -23,4 +23,7 @@ __all__ = [
     "OrtPackageManagers",
     "OrtRepositoryConfiguration",
     "OrtResolutions",
+    "Scanner",
+    "Severity",
+    "Storages",
 ]
