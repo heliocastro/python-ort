6a0506b98ec760884a0b14272e268e0adde5a931
Helio Chissini de Castro <helio.chissini.de.castro@cariad.technology>
2025-11-11 09:02:49 +0100

    chore(example): Simple repository configuration check
    Signed-off-by: Helio Chissini de Castro <helio.chissini.de.castro@cariad.technology>

---
+++ examples/any.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2025 Helio Chissini de Castro <heliocastro@gmail.com>
+# SPDX-License-Identifier: MIT
+#
+import logging
+import sys
+from pathlib import Path
+
+import click
+import yaml
+from pydantic import ValidationError
+from rich.pretty import pprint
+
+from ort import OrtRepositoryConfiguration
+
+logger = logging.getLogger()
+
+
+@click.command()
+@click.argument("datafile")
+def main(datafile: str) -> None:
+    try:
+        with Path(datafile).open() as fd:
+            data = yaml.safe_load(fd)
+        parsed = OrtRepositoryConfiguration(**data)
+        pprint(parsed)
+    except ValidationError as e:
+        logger.error(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
